f045f7922dc9e7ba35730e863d80ca88d00187b0
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-11-27 10:55:49 +0100

    Fully optimized cart control TODO apply to the rest

--- specs/e2e/cart/cartControl.spec.ts
+++ specs/e2e/cart/cartControl.spec.ts
@@ -3,202 +3,168 @@ import { cartControl } from "/Users/cmartinezm/Desktop/AutomationPlaywright/comp
 import { checkout } from "/Users/cmartinezm/Desktop/AutomationPlaywright/components/e2e/checkout.ts";
 import { loginRegister } from "/Users/cmartinezm/Desktop/AutomationPlaywright/components/e2e/loginRegister.ts";
 
-test.beforeEach("Setup", async ({ page }) => {
-  await page.goto("http://automationexercise.com");
-});
+test.describe("E2E Cart Control Tests", () => {
+  let cart: cartControl;
+  let check: checkout;
+  let loginReg: loginRegister;
 
-test("Test Case 12: Add products to cart", async ({ page }) => {
-  const cart = new cartControl(page);
+  test.beforeEach("Setup", async ({ page }) => {
+    await page.goto("http://automationexercise.com");
+    cart = new cartControl(page);
+    check = new checkout(page);
+    loginReg = new loginRegister(page);
+  });
 
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.addProduct();
-});
+  test("Test Case 12: Add products to cart", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.addProduct();
+  });
 
-test("Test Case 13: Add various of one product to cart", async ({ page }) => {
-  const cart = new cartControl(page);
+  test("Test Case 13: Add various of one product to cart", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.addManyProducts();
+  });
 
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.addManyProducts();
-});
+  test("Test Case 14: Sign up on checkout", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.addProduct();
 
-test("Test Case 14: Sign up on checkout", async ({ page }) => {
-  const cart = new cartControl(page);
-  const check = new checkout(page);
-  const loginReg = new loginRegister(page);
+    // Opens checkOut and signs up from the checkout Pop-up
+    await check.checkOut();
+    await check.checkoutSignUp();
 
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.addProduct();
+    await loginReg.signUp();
 
-  // Opens checkOut and signs up from the checkout Pop-up
-  await check.checkOut();
-  await check.checkoutSignUp();
+    // Goes to the cart page and checks out again, this time without sign up
+    await cart.openPage.openCartPage();
+    await check.checkOutVerifyAndBuy();
+    // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
+    await loginReg.deleteAccount();
+  });
 
-  await loginReg.signUp();
+  test("Test Case 15: Sign up before checkout", async () => {
+    await loginReg.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
 
-  // Goes to the cart page and checks out again, this time without sign up
-  await cart.openPage.openCartPage();
-  await check.checkOutVerifyAndBuy();
-  // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
-  await loginReg.deleteAccount();
-});
+    // Signs up beforehand using loginRegister.ts
+    await loginReg.signUp();
 
-test("Test Case 15: Sign up before checkout", async ({ page }) => {
-  const cart = new cartControl(page);
-  const check = new checkout(page);
-  const loginReg = new loginRegister(page);
+    // Buys products, we already signed up, so that step is skipped
+    await cart.openPage.openProductsPage();
+    // IMPORTANT adds product, since we skipped that part to sign up directly
+    await cart.addProduct();
+    await check.checkOutVerifyAndBuy();
+    // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
+    await loginReg.deleteAccount();
+  });
 
-  await loginReg.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
+  // THIS IS TO ALLOW CONTINOUS TESTING
+  test("DEBUG CASE 1: JUST sign up", async () => {
+    await loginReg.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
+    await loginReg.signUp();
+  });
 
-  // Signs up beforehand using loginRegister.ts
-  await loginReg.signUp();
+  // For this test is mandatory to have signed up WITHOUT deleting the account after
+  test("Test Case 16: LOG in before checkout", async () => {
+    await loginReg.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
 
-  // Buys products, we already signed up, so that step is skipped
-  await cart.openPage.openProductsPage();
-  // IMPORTANT adds product, since we skipped that part to sign up directly
-  await cart.addProduct();
-  await check.checkOutVerifyAndBuy();
-  // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
-  await loginReg.deleteAccount();
-});
+    // Logs in beforehand using loginRegister.ts
+    await loginReg.logIn();
 
-// THIS IS TO ALLOW CONTINOUS TESTING
-test("DEBUG CASE 1: JUST sign up", async ({ page }) => {
-  const loginReg = new loginRegister(page);
+    // Buys products, we already logged in, so that step is skipped
+    await cart.openPage.openProductsPage();
+    // IMPORTANT adds product, since we skipped that part to logged in directly
+    await cart.addProduct();
+    await check.checkOutVerifyAndBuy();
+    // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
+    await loginReg.deleteAccount();
+  });
 
-  await loginReg.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
-  await loginReg.signUp();
-});
+  test("Test Case 17: Remove products from cart", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.addProduct();
+    await cart.removeFromCart();
+  });
 
-// For this test is mandatory to have signed up WITHOUT deleting the account after
-test("Test Case 16: LOG in before checkout", async ({ page }) => {
-  const cart = new cartControl(page);
-  const check = new checkout(page);
-  const loginReg = new loginRegister(page);
+  test("Test Case 18: View Category Products", async () => {
+    await cart.openPage.goto();
+    await cart.verifyCategoriesAndBrands();
+    await cart.gotoWomenAndMen();
+  });
 
-  await loginReg.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
+  test("Test Case 19: View and Cart Brand Products", async () => {
+    await cart.openPage.goto();
+    await cart.verifyCategoriesAndBrands();
+    await cart.gotoWomenAndMen();
+    await cart.gotoBrands();
+  });
 
-  // Logs in beforehand using loginRegister.ts
-  await loginReg.logIn();
+  // THIS IS TO ALLOW CONTINOUS TESTING
+  test("DEBUG CASE 2: JUST sign up", async () => {
+    await loginReg.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
+    await loginReg.signUp();
+  });
 
-  // Buys products, we already logged in, so that step is skipped
-  await cart.openPage.openProductsPage();
-  // IMPORTANT adds product, since we skipped that part to logged in directly
-  await cart.addProduct();
-  await check.checkOutVerifyAndBuy();
-  // Deletes acc (Allows to loop the test without repeating credentials once you sign up again)
-  await loginReg.deleteAccount();
-});
+  test("Test Case 20: Search Products and Cart After Login", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.verification.verifyProductSearch();
+    await cart.addProduct();
+    await loginReg.openPage.openSignUpLoginPage();
+    await loginReg.logIn();
+    await cart.openPage.openCartPage();
+  });
 
-test("Test Case 17: Remove products from cart", async ({ page }) => {
-  const cart = new cartControl(page);
+  test("Test Case 21: Add review", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.reviewProduct();
+  });
 
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.addProduct();
-  await cart.removeFromCart();
-});
+  test("Test Case 22: Add to Cart from Recommended", async () => {
+    await cart.openPage.goto();
+    await cart.addRecommended();
+  });
 
-test("Test Case 18: View Category Products", async ({ page }) => {
-  const cart = new cartControl(page);
+  // THIS IS TO ALLOW CONTINOUS TESTING
+  test("DEBUG CASE 3: log and delete", async () => {
+    await loginReg.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
+    await loginReg.logIn();
+    await loginReg.deleteAccount();
+  });
 
-  await cart.openPage.goto();
-  await cart.verifyCategoriesAndBrands();
-  await cart.gotoWomenAndMen();
-});
+  // This test is a clone of Test case 15
+  // since we use credentials.ts, we already verify the adress details
+  test("Test Case 23: Verify Adress Details on Checkout", async () => {
+    await cart.openPage.goto();
+    await loginReg.openPage.openSignUpLoginPage();
+    await loginReg.signUp();
 
-test("Test Case 19: View and Cart Brand Products", async ({ page }) => {
-  const cart = new cartControl(page);
+    await cart.openPage.openProductsPage();
+    await cart.addProduct();
+    await check.checkOut();
 
-  await cart.openPage.goto();
-  await cart.verifyCategoriesAndBrands();
-  await cart.gotoWomenAndMen();
-  await cart.gotoBrands();
-});
+    await loginReg.deleteAccount();
+  });
 
-// THIS IS TO ALLOW CONTINOUS TESTING
-test("DEBUG CASE 2: JUST sign up", async ({ page }) => {
-  const loginReg = new loginRegister(page);
-
-  await loginReg.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
-  await loginReg.signUp();
-});
-
-test("Test Case 20: Search Products and Cart After Login", async ({ page }) => {
-  const cart = new cartControl(page);
-  const loginReg = new loginRegister(page);
-
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.verification.verifyProductSearch();
-  await cart.addProduct();
-  await loginReg.openPage.openSignUpLoginPage();
-  await loginReg.logIn();
-  await cart.openPage.openCartPage();
-});
-
-test("Test Case 21: Add review", async ({ page }) => {
-  const cart = new cartControl(page);
-
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.reviewProduct();
-});
-
-test("Test Case 22: Add to Cart from Recommended", async ({ page }) => {
-  const cart = new cartControl(page);
-
-  await cart.openPage.goto();
-  await cart.addRecommended();
-});
-
-// THIS IS TO ALLOW CONTINOUS TESTING
-test("DEBUG CASE 3: log and delete", async ({ page }) => {
-  const loginReg = new loginRegister(page);
-
-  await loginReg.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
-  await loginReg.logIn();
-  await loginReg.deleteAccount();
-});
-
-// This test is a clone of Test case 15
-// since we use credentials.ts, we already verify the adress details
-test("Test Case 23: Verify Adress Details on Checkout", async ({ page }) => {
-  const cart = new cartControl(page);
-  const check = new checkout(page);
-  const loginReg = new loginRegister(page);
-
-  await cart.openPage.goto();
-  await loginReg.openPage.openSignUpLoginPage();
-  await loginReg.signUp();
-
-  await cart.openPage.openProductsPage();
-  await cart.addProduct();
-  await check.checkOut();
-
-  await loginReg.deleteAccount();
-});
-
-test("Test Case 24: Verify Adress Details on Checkout", async ({ page }) => {
-  const cart = new cartControl(page);
-  const check = new checkout(page);
-  const loginReg = new loginRegister(page);
-
-  await cart.openPage.goto();
-  await cart.openPage.openProductsPage();
-  await cart.addProduct();
-  await check.checkOut();
-  await check.checkoutSignUp();
-  await loginReg.signUp();
-  await cart.openPage.openCartPage();
-  await check.checkOutVerifyAndBuy();
-  await check.downloadAndDelete();
-  await loginReg.deleteAccount();
+  test("Test Case 24: Verify Adress Details on Checkout", async () => {
+    await cart.openPage.goto();
+    await cart.openPage.openProductsPage();
+    await cart.addProduct();
+    await check.checkOut();
+    await check.checkoutSignUp();
+    await loginReg.signUp();
+    await cart.openPage.openCartPage();
+    await check.checkOutVerifyAndBuy();
+    await check.downloadAndDelete();
+    await loginReg.deleteAccount();
+  });
 });
